1f17630e282cf10314a550cd7d696d139dad056d
TeonaTanasa <teona.tanasa@principal33.com>
2021-09-15 00:15:23 +0300

    added SearchButtons code.

---
+++ src/components/Center/center-items/SearchButtons.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="search-buttons">
+    <button>{{searchButtonText}}</button>
+    <button>
+      <a v-bind:href="feelingLuckyButtonLink"> {{feelingLuckyButtonText}}</a>
+    </button>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      searchButtonText: "Căutare Google",
+      feelingLuckyButtonText: "Mă simt norocos",
+      feelingLuckyButtonLink: "https://www.google.com/doodles",
+    };
+  },
+};
+</script>
+
+<style>
+@import "../../styles/searchButtons.css";
+</style>
