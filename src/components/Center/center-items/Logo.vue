859f69691f9e100f35984f6c33332dcda81b7017
TeonaTanasa <teona.tanasa@principal33.com>
2021-09-14 23:52:24 +0300

    added Logo area code.

---
+++ src/components/Center/center-items/Logo.vue
@@ -0,0 +1,21 @@
+<template>
+    <div class="Google-Logo">
+        <img
+          v-bind:src="logo"
+          alt="Google Logo"
+          title="Google Search"
+        />
+      </div>
+</template>
+
+<script>
+import googleLogo from "../../../assets/Google-Logo.png";
+// fara import nu merge imaginea folosing v-bind chiar daca ii dau path-ul mai jos
+export default {
+  data() {
+    return {
+      logo: googleLogo,
+    };
+  },
+};
+</script>
